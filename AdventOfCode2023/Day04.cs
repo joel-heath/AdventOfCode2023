ce9c0c529fee6a092bf06373ab6aea2b04d1e5db
Joel Heath <joelheath@outlook.com>
2023-12-04 05:19:04 +0000

    Day 4 solution

--- AdventOfCode2023/Day04.cs
+++ AdventOfCode2023/Day04.cs
@@ -4,22 +4,54 @@ public class Day04 : IDay
     public int Day => 4;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", "13" }
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", "30" }
     };
 
     public string SolvePart1(string input)
     {
-        return string.Empty;
+        var total = 0;
+
+        var lines = input.Split(Environment.NewLine);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var sides = line.Split(" | ").Select(i => i.Split(": ").Last()).Select(s => s.Split(' ').Select(e => e.Trim()).Where(i => i != " " && i != "")).ToArray();
+
+            var score = (int)Math.Pow(2, sides[0].Intersect(sides[1]).Count() - 1);
+
+            total += score;
+        }
+
+        return $"{total}";
     }
 
     public string SolvePart2(string input)
     {
-        return string.Empty;
+        var count = 0;
+        var lines = input.Split(Environment.NewLine);
+        var instances = Enumerable.Range(0, lines.Length).ToDictionary(i => i, i => 1);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var line = lines[i];
+            var sides = line.Split(" | ").Select(i => i.Split(": ").Last()).Select(s => s.Split(' ').Select(e => e.Trim()).Where(i => i != " " && i != "")).ToArray();
+
+            var score = sides[0].Intersect(sides[1]).Count();
+
+            Console.WriteLine(score);
+            for (int j = 0; j < instances[i]; j++)
+            {
+                for (int k = 1; k <= score; k++)
+                    instances[i + k]++;
+            
+                count++;
+            }
+        }
+
+        return $"{instances.Sum(i => i.Value)}";
     }
 }
