66077d4e3e662babc744f65e06acb1f625ce7521
Joel Heath <joelheath@outlook.com>
2023-12-06 20:21:48 +0000

    Day 5 big optimisations, no ToList or ToArrays (or ToHashSet) anywhere, and (as far as I know) no repeated evaluation / enumeration over IEnumerable

--- AdventOfCode2023/Day05.cs
+++ AdventOfCode2023/Day05.cs
@@ -14,35 +14,33 @@ public class Day05 : IDay
     public string SolvePart1(string input)
         => $"{new string[][] { input.Split(Environment.NewLine + Environment.NewLine) }
             .Select(b =>
-                new (long[], string[])[] { (b[0].Split(" ")[1..].Select(long.Parse).ToArray(), b[1..]) }
-                .Select<(long[] seeds, string[] maps), long>(i =>
-                    new IEnumerable<HashSet<(long destination, long source, long range)>>[] { i.maps.Select(m => m.Split(Environment.NewLine)[1..].Select(l => l.Split(" ").Select(long.Parse).ToArray())
-                                .Select<long[], (long destination, long source, long range)>(d => (d[0], d[1], d[2])).ToHashSet()) }
-                                .Select(mappings =>
-                                    i.seeds.Select(s => 
-                                        mappings.Aggregate(s, (seedAcc, m) =>
-                                            new List<(long destination, long source, long range)> { m.FirstOrDefault(m => m.source <= seedAcc && seedAcc <= m.source + m.range) }
-                                            .Select(m => seedAcc - m.source + m.destination).First()))
-                                    .Min())
-                                .First())
-                    .First()).First()}";
+                new (IEnumerable<long>, string[])[] { (b[0].Split(" ")[1..].Select(long.Parse), b[1..]) }
+                .SelectMany<(IEnumerable<long> seeds, string[] maps), long>(i =>
+                    new IEnumerable<IEnumerable<(long destination, long source, long range)>>[] {
+                        i.maps.Select(m => m.Split(Environment.NewLine)[1..].Select(l => l.Split(" ").Select(long.Parse).GetEnumerator())
+                              .Select(d => (d.MoveNext() ? d.Current : 0, d.MoveNext() ? d.Current : 0, d.MoveNext() ? d.Current : 0))) }
+                        .Select(mappings => i.seeds.Min(s =>
+                            mappings.Aggregate(s, (seedAcc, m) =>
+                                new (long destination, long source, long range)[] { m.FirstOrDefault(m => m.source <= seedAcc && seedAcc <= m.source + m.range) }
+                                .Select(m => seedAcc - m.source + m.destination).First())))).First()).First()}";
 
     public string SolvePart2(string input)
-    => $"{new string[][] { input.Split(Environment.NewLine + Environment.NewLine) }
-        .Select(b => (b[0].Split(" ")[1..].Select(long.Parse).Chunk(2).Select(i => (i[0], i[1])).ToList(), b[1..]))
-        .Select<(List<(long source, long range)> seeds, string[] maps), long>(i =>
-            i.maps.Select(m => m.Split(Environment.NewLine)[1..]
-                      .Select(l => l.Split(" ").Select(long.Parse).ToArray())
-                      .Select<long[], (long destination, long source, long range)>(d => (d[0], d[1], d[2])).ToList())
-                  .ToList()
-                  .Aggregate(i.seeds, (acc, m) => [.. CalculateNewSeeds(acc, m.OrderBy(s => s.source).ToList())])
-                  .Min(x => x.source)).First()}";
+        => $"{new string[][] { input.Split(Environment.NewLine + Environment.NewLine) }
+            .Select(b => (b[0].Split(" ")[1..].Select(long.Parse).Chunk(2).Select(i => (i[0], i[1])), b[1..]))
+            .Select<(IEnumerable<(long source, long range)> seeds, string[] maps), long>(i =>
+                i.maps.Select(m => m.Split(Environment.NewLine)[1..]
+                            .Select(l => l.Split(" ").Select(long.Parse).GetEnumerator())
+                            .Select<IEnumerator<long>, (long destination, long source, long range)>(d => (d.MoveNext() ? d.Current : 0, d.MoveNext() ? d.Current : 0, d.MoveNext() ? d.Current : 0)).OrderBy(s => s.source))
+                        .Aggregate(i.seeds, (acc, m) => CalculateNewSeeds(acc.GetEnumerator(), m))
+                        .Min(x => x.source)).First()}";
 
-    public IEnumerable<(long source, long range)> CalculateNewSeeds(List<(long source, long range)> seeds, IReadOnlyList<(long destination, long source, long range)> mappings)
-        => (seeds.Count == 0) ? [] : new List<(long destination, long source, long range)>() { mappings.FirstOrDefault(m => !(seeds[0].source + seeds[0].range <= m.source || seeds[0].source >= m.source + m.range)) }
+    public IEnumerable<(long source, long range)> CalculateNewSeeds(IEnumerator<(long source, long range)> seeds, IEnumerable<(long destination, long source, long range)> mappings)
+        => (!seeds.MoveNext()) ? [] : new (long destination, long source, long range)[] { mappings.FirstOrDefault(m => !(seeds.Current.source + seeds.Current.range <= m.source || seeds.Current.source >= m.source + m.range)) }
             .SelectMany(m => m == (0, 0, 0)
-                ? new List<(long source, long range)>() { (seeds[0].source, seeds[0].range) }.Concat(CalculateNewSeeds(seeds.Count > 1 ? seeds[1..] : [], mappings)) // [1..]
-                : (seeds[0].source < m.source ? new List<(long source, long range)>() { (seeds[0].source, m.source) } : [])
-                    .Append((m.destination + Math.Max(seeds[0].source, m.source) - m.source, Math.Min(seeds[0].source + seeds[0].range, m.source + m.range) - Math.Max(seeds[0].source, m.source)))
-                    .Concat(CalculateNewSeeds((seeds.Count > 1 ? seeds[1..] : []).Concat(m.source + m.range < seeds[0].source + seeds[0].range ? new List<(long source, long range)>() { (m.source + m.range, seeds[0].range - (m.source + m.range - Math.Max(seeds[0].source, m.source))) } : []).ToList(), mappings)));
+                ? new (long source, long range)[] { (seeds.Current.source, seeds.Current.range) }.Concat(CalculateNewSeeds(seeds, mappings))
+                : (seeds.Current.source < m.source ? new (long source, long range)[] { (seeds.Current.source, m.source) } : [])
+                    .Append((m.destination + Math.Max(seeds.Current.source, m.source) - m.source, Math.Min(seeds.Current.source + seeds.Current.range, m.source + m.range) - Math.Max(seeds.Current.source, m.source)))
+                    .Concat(CalculateNewSeeds((m.source + m.range < seeds.Current.source + seeds.Current.range ? new List<(long source, long range)> { (m.source + m.range, seeds.Current.range - (m.source + m.range - Math.Max(seeds.Current.source, m.source))) } : []).GetEnumerator(), mappings))
+                    .Concat(CalculateNewSeeds(seeds, mappings)));
+
 }
--- AdventOfCode2023/ExtensionMethods.cs
+++ AdventOfCode2023/ExtensionMethods.cs
@@ -6,14 +6,4 @@ public static class ExtensionMethods
         Console.WriteLine(input);
         return input;
     }
-
-    public static IReadOnlyList<IReadOnlyList<T>> Transpose<T>(this IReadOnlyList<IReadOnlyList<T>> source)
-    {
-        return Enumerable.Range(0, source[0].Count)
-          .Select(c => source
-             .Where(line => line != null && line.Count > c)
-             .Select(line => line[c])
-             .ToArray())
-          .ToArray();
-    }
 }
